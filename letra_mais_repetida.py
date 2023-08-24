420e7e4fe469865dafb512c09bc4b04ae6b22351
Your Name <you@example.com>
2023-08-23 21:34:12 -0300

    ...

--- letra_mais_repetida.py
+++ letra_mais_repetida.py
@@ -3,32 +3,26 @@ print('\n')
 
 print('****** QUAL LETRA APARECE MAIS NA FRASE QUE VOCÊ ESCRECEU? ****** \n')
 
-    frase = input('Digite uma frase >>: ')
+frase = input('Digite uma frase >>: ')
 
-    largura_frase_digitada = len(frase)
-    contador = 0
+largura_frase_digitada = len(frase)
+contador = 0
 
 
-    print('\n')
+print('\n')
 
-    print(f'Sua frase tem {largura_frase_digitada} caracteres \n')
+print(f'Sua frase tem {largura_frase_digitada} caracteres \n')
 
-    while contador <= (largura_frase_digitada - 1):
+while contador <= (largura_frase_digitada - 1):
 
-        caractere = frase[contador]
-        caractere_anterior = frase[contador - 1]
-        caractere_mais_repetido = 0
+    caractere = frase[contador]
+    caractere_anterior = frase[contador - 1]
+    caractere_mais_repetido = 0
 
-        if frase.count(caractere_anterior) > frase.count(caractere):
-            caractere_mais_repetido = caractere_anterior
-            print(f'O Caractere mais repetido é: {caractere_mais_repetido} e apareceu {frase.count(caractere_mais_repetido)}')
-        else:
-            ...
+    if frase.count(caractere_anterior) > frase.count(caractere):
+        caractere_mais_repetido = caractere_anterior
+        print(f'O Caractere mais repetido é: {caractere_mais_repetido} e apareceu {frase.count(caractere_mais_repetido)}')
+    else:
+        ...
 
-        contador += 1
-
-elif menu == '2':
-    print('Você escolheu a opção 2')
-
-else:
-    print('você digitou opções diferentes de 1 e 2 \n')
+    contador += 1
